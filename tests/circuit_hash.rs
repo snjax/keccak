f2f6b65554246a1facf9fce6a2f2ef7b3ea34573
Igor Gulamov <igor.gulamov@gmail.com>
2023-02-09 16:32:42 +0400

    Add larger tests

--- tests/circuit_hash.rs
+++ tests/circuit_hash.rs
@@ -51,6 +51,11 @@ fn test_circuit_long_value() {
     perform_test(data);
 }
 
+#[test]
+fn test_circuit_long_value_2() {
+    let data = b"In computer science, a perfect hash function h for a set S is a hash function that maps distinct elements in S to a set of m integers, with no collisions. In mathematical terms, it is an injective function. Perfect hash functions may be used to implement a lookup table with constant worst-case access time. A perfect hash function can, as any hash function, be used to implement hash tables, with the advantage that no collision resolution has to be implemented. In addition, if the keys are not the data and if it is known that queried keys will be valid, then the keys do not need to be stored in the lookup table, saving space. Disadvantages of perfect hash functions are that S needs to be known for the construction of the perfect hash function. Non-dynamic perfect hash functions need to be re-constructed if S changes. For frequently changing S dynamic perfect hash functions may be used at the cost of additional space.[1] The space requirement to store the perfect hash function is in O(n). The important performance parameters for perfect hash functions are the evaluation time, which should be constant, the construction time, and the representation size.";
+    perform_test(data);
+}
 
 
 #[test]
--- tests/hash.rs
+++ tests/hash.rs
@@ -12,6 +12,13 @@ fn test_long_value() {
     let data = b"A perfect hash function for a specific set S that can be evaluated in constant time, and with values in a small range, can be found by a randomized algorithm in a number of operations";
     assert_eq!(encode(&keccak256(data)), "60d820b069bdb4545c690a11e8f8e6e444878010228920dd1598925a8fc2edcf");
 }
+
+#[test]
+fn test_long_value_2() {
+    let data = b"In computer science, a perfect hash function h for a set S is a hash function that maps distinct elements in S to a set of m integers, with no collisions. In mathematical terms, it is an injective function. Perfect hash functions may be used to implement a lookup table with constant worst-case access time. A perfect hash function can, as any hash function, be used to implement hash tables, with the advantage that no collision resolution has to be implemented. In addition, if the keys are not the data and if it is known that queried keys will be valid, then the keys do not need to be stored in the lookup table, saving space. Disadvantages of perfect hash functions are that S needs to be known for the construction of the perfect hash function. Non-dynamic perfect hash functions need to be re-constructed if S changes. For frequently changing S dynamic perfect hash functions may be used at the cost of additional space.[1] The space requirement to store the perfect hash function is in O(n). The important performance parameters for perfect hash functions are the evaluation time, which should be constant, the construction time, and the representation size.";
+    assert_eq!(encode(&keccak256(data)), "7c9232f4303dcbf7a0121c622154441549050e85b29997ef38fe8e42e745f5db");
+}
+
 #[test]
 fn test_bitrate_sized_value() {
     let data = b"A perfect hash function for a specific set S that can be evaluated in constant time, and with values in a small range, can be found by a";
